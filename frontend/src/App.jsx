25f88d87be4f5dc51c75a2fa277cac5d255056cc
jatin39-bit <techrevain@gmail.com>
2025-02-15 11:29:34 +0530

    corrections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import AppRoutes from './routes/AppRoutes'
 import './App.css'
 import UserProvider from './context/user.context'
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return(
+    <BrowserRouter>
     <UserProvider>
       <AppRoutes />
     </UserProvider>
+    </BrowserRouter>
   )
 }
 
