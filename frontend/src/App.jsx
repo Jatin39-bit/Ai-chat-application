9c68ea4f82a5779a75b5d2c7bce6babc52c12670
Jatin39-bit <techrevain@gmail.com>
2025-02-15 11:32:22 +0530

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import AppRoutes from './routes/AppRoutes'
 import './App.css'
 import UserProvider from './context/user.context'
-import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return(
-    <BrowserRouter>
     <UserProvider>
       <AppRoutes />
     </UserProvider>
-    </BrowserRouter>
   )
 }
 
